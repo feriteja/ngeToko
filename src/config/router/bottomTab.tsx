497875f335935bd4284f891ed0304ef2b8ab883b
feriteja <feriteja@gmail.com>
2021-05-18 16:38:56 +0700

    give icon to bottomnav

--- src/config/router/bottomTab.tsx
+++ src/config/router/bottomTab.tsx
@@ -4,6 +4,7 @@ import {NavigatorScreenParams} from '@react-navigation/native';
 import DashBoard from './sideBar';
 import {Cart, Profile} from '../../screens';
 import {dashBoardStackParamList} from './sideBar';
+import IconFa from 'react-native-vector-icons/FontAwesome';
 
 export type bottomStackParamList = {
   dashBoard: NavigatorScreenParams<dashBoardStackParamList>;
@@ -16,9 +17,36 @@ const BottomTab = createBottomTabNavigator<bottomStackParamList>();
 const bottomTab = () => {
   return (
     <BottomTab.Navigator>
-      <BottomTab.Screen name="dashBoard" component={DashBoard} />
-      <BottomTab.Screen name="cart" component={Cart} />
-      <BottomTab.Screen name="profile" component={Profile} />
+      <BottomTab.Screen
+        name="dashBoard"
+        component={DashBoard}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <IconFa name="delicious" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="cart"
+        component={Cart}
+        options={{
+          tabBarLabel: 'Keranjang',
+          tabBarIcon: ({color, size}) => (
+            <IconFa name="opencart" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({color, size}) => (
+            <IconFa name="user-o" color={color} size={size} />
+          ),
+        }}
+      />
     </BottomTab.Navigator>
   );
 };
--- src/screens/main/dashBoard/dashBoard.tsx
+++ src/screens/main/dashBoard/dashBoard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-const dashBoard = () => {
+const dashBoard = ({}) => {
   return (
     <View>
       <Text>dashboard</Text>
