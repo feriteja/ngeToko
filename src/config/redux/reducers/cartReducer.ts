500335b392222c867b5640120a4783a1e0062faf
feriteja <feriteja@gmail.com>
2021-05-21 02:51:32 +0700

    simplefy redux

--- src/config/redux/reducers/cartReducer.ts
+++ src/config/redux/reducers/cartReducer.ts
@@ -1,15 +1,28 @@
-import {itemAction, itemState} from '../../../constant/type/reduxType';
+import {cart} from '../../../constant/type/itemType';
+import {CartAction, CartState} from '../../../constant/type/reduxType';
 import * as actionSTATE from '../actions/index';
 
-const initialState: itemState = [];
+const initialState: CartState = [];
 
-export default function (state: itemState = initialState, action: itemAction) {
+export default function (state = initialState, action: CartAction) {
   switch (action.type) {
-    case actionSTATE.GET_ITEM:
-      return [...action.payload];
+    case actionSTATE.GET_CART:
+      if (Array.isArray(action.payload)) {
+        return [...action.payload];
+      }
 
-    case actionSTATE.CLEAR_ITEM:
-      return [];
+    case actionSTATE.ADD_CART_ITEM:
+      const findIDX = state.findIndex(
+        x => x.item.uid === action.payload.item.uid,
+      );
+
+      const addArrayNew = [...state];
+      if (findIDX >= 0) {
+        addArrayNew[findIDX] = action.payload;
+        return addArrayNew;
+      } else {
+        return [...state, {...action.payload}];
+      }
 
     default:
       return state;
